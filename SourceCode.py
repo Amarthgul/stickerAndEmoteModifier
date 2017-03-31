d30018f2039607ac37ce50ff2aa2e50893743442
Amarth Gul <amarthgul@gmail.com>
2017-03-30 20:16:33 -0700

    Update SourceCode.py

--- SourceCode.py
+++ SourceCode.py
@@ -7,44 +7,58 @@ This script is orignally coded to resize images for telegram stickers making
                     in which has a pile of images resized.
                         (You may need install pillow)
 '''
-
+import timeit
 import PIL.Image as img
 import os
+import re
+
+maxsize = 512 #You can change this size tofit your own need
 
 def reSize(inlist):
-    smallEdge = int( min(inlist[0], inlist[1]) * 512 / max(inlist[0], inlist[1]))
+    smallEdge = int( min(inlist[0], inlist[1]) * maxsize / max(inlist[0], inlist[1]))
     if inlist[0] > inlist[1]:
-        return 512, smallEdge
+        return maxsize, smallEdge
     elif inlist[0] < inlist[1]:
-        return smallEdge, 512
+        return smallEdge, maxsize
     elif inlist[0] == inlist[1]:
-        return 512, 512
-    
+        return maxsize, maxsize
+
 def GetPathAndResize():
+    picType = 'png|jpg|gif|bmp|psd|tif'
     current = str(os.path.abspath(__file__))
-    for itera in range(len(current) - 1, 0, -1): #I'm not familiar with re yet, so I use a C liked way to remove NAME.py
+    for itera in range(len(current) - 1, 0, -1):
         if current[itera] == '\\':
-            dir = current[0: itera]
+            dir = current[0: itera]#Get current directory
             break;
-    files = os.listdir(dir)#Same reason, remove NAME.py
+    files = os.listdir(dir)
+    images = []
     for itera in range(len(files)):
-        if 'py' in str(files[itera]):
-            temp = files[itera]
-            files.remove(temp)
-            break;
-    #print(files)
+        if re.search(picType, str(files[itera])):
+            images.append(str(files[itera]))
+    print('All Images Found:\n' + str(images))
     outDir = dir + '\\OutputImages\\'
     if not os.path.exists(outDir):
         os.makedirs(outDir)
     #============================================================
-    for itera in range(len(files)):
-        targetLoc = dir + '\\' + files[itera]
+    for itera in range(len(images)):
+        targetLoc = dir + '\\' + images[itera]
         print(targetLoc)
         target = img.open(targetLoc)
         xEdge, yEdge = reSize(target.size)
         out = target.resize((xEdge, yEdge), img.ANTIALIAS)
         out.save(outDir+ str(itera) +'outImage.png','PNG')
-        
-        
+    
+def Main():  
+    try:
+        start = timeit.default_timer()
+
+        GetPathAndResize()
+
+        end = timeit.default_timer()
+        print('Time costed:',end - start)
+    except (ValueError,NameError,SyntaxError,AttributeError,\
+        TypeError) as err:
+        print('Error!',err);
+
 if __name__ == '__main__':
-    GetPathAndResize()
+    Main()
